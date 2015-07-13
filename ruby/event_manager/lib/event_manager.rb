7f13291721a0dd1485c189c928f2893d0c3c723b
Aaron Sluis <amsluis@gmail.com>
2015-07-12 21:05:23 -0700

    basic csv parser for event_manager

--- ruby/event_manager/lib/event_manager.rb
+++ ruby/event_manager/lib/event_manager.rb
@@ -1,10 +1,16 @@
+require 'csv'
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5,'0')[0..4]
+end
+
 puts "~~~Event Manager Initialized~~~\n\n"
 
-lines = File.readlines "/home/aaron/the_odin_project/ruby/event_manager/event_attendees.csv"
+contents = CSV.open "/home/aaron/the_odin_project/ruby/event_manager/event_attendees.csv", headers: true, header_converters: :symbol
+contents.each do |row|
+  name = row[:first_name]      # use symbols for header column titles by specifying header_converters
+  zipcode = clean_zipcode(row[:zipcode])
 
-lines.each_with_index do |line, index|
-  next if index == 0
-  columns = line.split(',')
-  name = columns[2]
-  puts name
+  puts "#{name} #{zipcode}"
 end
+
