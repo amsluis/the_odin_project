50df1cc482bb86964a79c0807ba65dd401edd6de
Aaron Sluis <amsluis@gmail.com>
2016-09-01 16:25:24 -0700

    adds formatting and finishes web guesser project

--- rails/guesser/web_guesser.rb
+++ rails/guesser/web_guesser.rb
@@ -1,7 +1,23 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-magic_num = rand(101)
+$magic_num = rand(101)
+
 get '/' do
-  "The secret number is #{magic_num}"
+  guess = params["guess"]
+  message, format= check_guess(guess)
+  erb :index, :locals => {:number => $magic_num, :message => message, :format => format}
+end
+
+def check_guess(guess)
+  guess = guess.to_i
+  if guess.nil?
+    return "Please enter a number", "background: gray;"
+  elsif guess > $magic_num
+    return "Guess is too high", "background: red;"
+  elsif guess < $magic_num
+    return "Guess is too low", "background: blue;"
+  elsif guess == $magic_num
+    return "Correct!", "background: green;"
+  end
 end
